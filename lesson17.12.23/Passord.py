fcc4ea8fe18db08d924dc70851716c748f4a0541
Piwko1423 <154062772+Piwko1423@users.noreply.github.com>
2023-12-17 17:43:25 +0400

    создали папку с занятием 17.12

---
+++ lesson17.12.23/Passord.py
@@ -0,0 +1,23 @@
+def ask_password():
+    password = input('Ведите пароль')
+    if password == 'password':
+        print('пароль совпадает')
+    else:
+        print('пароль не совпадает')
+ask_password()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
