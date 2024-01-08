4bcfb40ef578d3f9caabb917020cc10e8d544529
Piwko1423 <154062772+Piwko1423@users.noreply.github.com>
2024-01-08 17:51:10 +0400

    vмини игры

--- lesspn24.12.23/shahki.py
+++ lesspn24.12.23/shahki.py
@@ -1,10 +1,21 @@
-import tkinter
+import  tkinter
+
+print('start')
 win = tkinter.Tk()
 
-canvas = tkinter.Canvas(win,bg = '#fff', width=400 , height=400 )
+canvas = tkinter.Canvas(win, bg='white', width=400, height=400)
+canvas.create_line((100, 100), (100,150), fill='black')
+canvas.create_line((300,300 ), (300,240), fill='black')
+canvas.create_line((320, 320), (320,240), fill='black')
+canvas.create_line((360, 100), (360,150), fill='black')
+canvas.create_line((380, 100), (380,150), fill='black')
+canvas.create_line((400, 100), (400,150), fill='black')
+canvas.create_line((100, 100), (100,150), fill='black')
+canvas.create_line((100, 100), (100,150), fill='black')
+canvas.create_line((100, 100), (100,150), fill='black')
+canvas.create_line((100, 100), (100,150), fill='black')
+
+
 canvas.pack()
-for i in range(1,8):
-    canvas.create_line((i * 50,0),(i * 50,400),fill='black')
-for i in range(1,8):
-    canvas.create_line((0, i * 50),(400, i * 50),fill='black')
+
 win.mainloop()
