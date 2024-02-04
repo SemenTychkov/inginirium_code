a7dda188ab7eaec6a794d39221b5bbb4644bc385
Piwko1423 <154062772+Piwko1423@users.noreply.github.com>
2024-02-04 17:44:50 +0400

    ООП

---
+++ Lesson04.02.24/main.py
@@ -0,0 +1,16 @@
+class Car:
+       def __init__(self,color,mark,max_speed,weight):
+           self.color=color
+           self.mark = mark
+           self.max_speed = max_speed
+           self.weight =weight
+
+
+
+
+Lada = Car('Зеленая','Ламборгини','666','400')
+
+print(Lada.color)
+print(Lada.mark)
+print(Lada.max_speed)
+print(Lada.weight)
--- Lesson28.01.24/main.py
+++ Lesson28.01.24/main.py
@@ -1,47 +1,12 @@
-import pygame
+let = int(input("Введите Ваш Месяц Рождения:"))
+if let == str(12) or let == str(1) or let == str(2):
 
-pygame.init()
-
-width = 500
-height = 500
-
-win = pygame.display.set_mode((width,height))
-
-xCircle = width / 2
-yCircle = height / 2
-cx = width / 2
-cy = height / 2
-color =(0,255,255)
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-    keys = pygame.key.get_pressed()
-    if  keys[pygame.K_LEFT]:
-        xCircle-=5
-    elif  keys[pygame.K_RIGHT]:
-        xCircle+=5
-    elif  keys[pygame.K_UP]:
-        yCircle-=5
-    elif  keys[pygame.K_DOWN]:
-        yCircle+=5
-    else:
-        xCircle = width / 2
-        yCircle = height / 2
-    if xCircle >cx + 150:
-       color = (255,255,0)
-    elif xCircle <cx - 150:
-       color = (255,255,0)
-    elif yCircle >cy + 150:
-       color = (255,255,0)
-    elif yCircle < cy - 150:
-           color = (255, 255, 0)
-    else:
-        
-
-
-
-    win.fill((25,200,50))
-    pygame.draw.circle(win,color,(xCircle,yCircle),30)
-    pygame.display.update()
-    pygame.time.delay(10)
+     print("За окном выпал первый снег")
+elif let == str(3) or let == str(4) or let == str(5):
+    print("Птицы пели прекрасные песни")
+elif let == str(6) or let == str(7) or let == str(8):
+        print("Сольнце светило ярче чем когда либо")
+elif let == str(9) or let == str(10) or let == str(11):
+    print("Урожай был невероятный")
+else:
+    print("Нету такого месяца")
